b014311f09287a959b3713ce5ce6e95d7069ab61
KimKiHong-1111 <rlghd0157@gmail.com>
2025-07-14 18:14:57 +0900

    "프로젝트 초기 세팅"

---
+++ src/main/java/api/orderagent/OrderAgentApplication.java
@@ -0,0 +1,13 @@
+package api.orderagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/orderagent/OrderAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package api.orderagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
